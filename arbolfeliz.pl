fe2964838d48c12e118c3d45ab795eb3cc1c3cc3
GabrielaAntolinez <36423718+GabrielaAntolinez@users.noreply.github.com>
2018-05-07 16:59:58 -0500

    Add files via upload

---
+++ arbolfeliz.pl
@@ -0,0 +1,34 @@
+padreDe('Filomena','Gabriela').
+padreDe('Filomena','Alexandra').
+padreDe('Filomena','Angela').
+padreDe('Rodrigo','Gabriela').
+padreDe('Rodrigo','Alexandra').
+padreDe('Rodrigo','Angela').
+padreDe('Carmen','Filomena').
+padreDe('Mary','Edward').
+padreDe('Mary','Brayan').
+padreDe('Carmen','Mary').
+padreDe('Patricia','Laura').
+padreDe('Patricia','Cristian').
+padreDe('Carmen','Patricia').
+padreDe('MaximilianoJr','Filomena').
+padreDe('MaximilianoJr','Mary').
+padreDe('MaximilianoJr','Patricia').
+padreDe('Maximiliano','MaximilianoJr').
+padreDe('Maximiliano','Florentino').
+padreDe('Maximiliano','Misael').
+padreDe('Paulina','Misael').
+padreDe('Paulina','Florentino').
+padreDe('Paulina','MaximilianoJr').
+
+hermanosde(A,B):- padreDe(C,A),padreDe(C,B),A\==B.
+hijode(A,B):-padreDe(B,A).
+abuelode(A,B):- padreDe(A,C), padreDe(C,B).
+bisabuelode(A,B):- padreDe(A,D),padreDe(D,C),padreDe(C,B).
+tiode(A,B):- padreDe(C,B),hermanosde(A,C),A\==B.
+primode(A,B):- hermanosde(C,D),padreDe(C,A),padreDe(D,B).
+
+parejade(A,B):-hijode(X,A),padreDe(B,X), A \== B.
+suegrode(A,B):- padreDe(A,C),parejade(C,B).
+cuñadode(A,B):-hermanosde(A,C),parejade(C,B).
+feliz(A):-not(parejade(A,_)).
